74bc3577ad737f3310d014789da5b0a2a84ef884
vkotsiuba99 <92172262+vkotsiuba99@users.noreply.github.com>
2022-09-15 20:52:52 +0300

    account-api: add get params for user route

--- account-api/handlers/get.go
+++ account-api/handlers/get.go
@@ -1,8 +1,15 @@
 package handlers
 
-import "net/http"
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+)
 
 // ListSingle handles get requests and returns single user by id
 func (p *Accounts) ListSingle(rw http.ResponseWriter, r *http.Request) {
 	// TODO: get single account from database
+	params := mux.Vars(r)
+	id := params["id"]
+
+	p.logger.Println(id)
 }

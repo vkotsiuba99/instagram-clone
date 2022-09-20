378701624eb3514407b6312c56249f7b820914be
vkotsiuba99 <92172262+vkotsiuba99@users.noreply.github.com>
2022-09-20 19:24:30 +0300

    add env var to jwt secret

--- internal/auth.go
+++ internal/auth.go
@@ -1,9 +1,12 @@
 package internal
 
-import "github.com/dgrijalva/jwt-go"
+import (
+	"github.com/dgrijalva/jwt-go"
+	"os"
+)
 
 // JWTKey is used to create the signature
-var JWTKey = []byte("secret_key") // TODO: refactor to env variable
+var JWTKey = []byte(os.Getenv("JWT_SECRET_KEY"))
 
 // Claims is a struct that will be encoded in a jwt
 type Claims struct {
